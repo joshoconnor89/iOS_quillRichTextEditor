2148c060b199b583d7cb69d864274cdc1166c0cc
Josh O'Connor <josh.0@Joshs-Workstation.local>
2017-05-24 10:16:34 -0700

    init

---
+++ quill_rich_text_editor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quill_rich_text_editor
+//
+//  Created by Joshua O'Connor on 5/24/17.
+//  Copyright © 2017 Josh O'COnnor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
